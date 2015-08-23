e1cdb68f46e85a3fa54b76b29789bff685eb9fb1
azmi <azmi.stuff@gmail.com>
2015-08-23 22:35:48 +0800

    Initial commit

---
+++ M101Python.BottleWebApp/routes.py
@@ -0,0 +1,288 @@
+﻿"""
+Routes and views for the bottle application.
+"""
+
+import bottle
+from datetime import datetime
+import cgi
+import re
+import pymongo
+import blogPostDAO
+import sessionDAO
+import userDAO
+from helpers import validate_signup, extract_tags
+
+connection_string = "mongodb://localhost"
+connection = pymongo.MongoClient(connection_string)
+database = connection.blog
+
+posts = blogPostDAO.BlogPostDAO(database)
+users = userDAO.UserDAO(database)
+sessions = sessionDAO.SessionDAO(database)
+
+#@bottle.route('/')
+#@bottle.route('/home')
+#@bottle.view('index')
+#def home():
+#    """Renders the home page."""
+#    return dict(
+#        year=datetime.now().year
+#    )
+
+#@bottle.route('/contact')
+#@bottle.view('contact')
+#def contact():
+#    """Renders the contact page."""
+#    return dict(
+#        title='Contact',
+#        message='Your contact page.',
+#        year=datetime.now().year
+#    )
+
+#@bottle.route('/about')
+#@bottle.view('about')
+#def about():
+#    """Renders the about page."""
+#    return dict(
+#        title='About',
+#        message='Your application description page.',
+#        year=datetime.now().year
+#    )
+
+@bottle.route('/')
+@bottle.view('blog_template')
+def blog_index():
+
+    cookie = bottle.request.get_cookie("session")
+    username = sessions.get_username(cookie)
+
+    # even if there is no logged in user, we can show the blog
+    l = posts.get_posts(10)
+
+    return dict(
+        myposts=l,
+        username=username,
+        title='Home page',
+        year=datetime.now().year
+    )
+
+
+# Displays a particular blog post
+@bottle.get("/post/<permalink>")
+def show_post(permalink="notfound"):
+
+    cookie = bottle.request.get_cookie("session")
+
+    username = sessions.get_username(cookie)
+    permalink = cgi.escape(permalink)
+
+    print "about to query on permalink = ", permalink
+    post = posts.get_post_by_permalink(permalink)
+
+    if post is None:
+        bottle.redirect("/post_not_found")
+
+    # init comment form fields for additional comment
+    comment = {'name': "", 'body': "", 'email': ""}
+
+    return bottle.template("entry_template", dict(post=post, username=username, errors="", comment=comment))
+
+
+# used to process a comment on a blog post
+@bottle.post('/newcomment')
+def post_new_comment():
+    name = bottle.request.forms.get("commentName")
+    email = bottle.request.forms.get("commentEmail")
+    body = bottle.request.forms.get("commentBody")
+    permalink = bottle.request.forms.get("permalink")
+
+    post = posts.get_post_by_permalink(permalink)
+    cookie = bottle.request.get_cookie("session")
+
+    username = sessions.get_username(cookie)
+
+    # if post not found, redirect to post not found error
+    if post is None:
+        bottle.redirect("/post_not_found")
+        return
+
+    # if values not good, redirect to view with errors
+
+    if name == "" or body == "":
+        # user did not fill in enough information
+
+        # init comment for web form
+        comment = {'name': name, 'email': email, 'body': body}
+
+        errors = "Post must contain your name and an actual comment."
+        return bottle.template("entry_template", dict(post=post, username=username, errors=errors, comment=comment))
+
+    else:
+
+        # it all looks good, insert the comment into the blog post and redirect back to the post viewer
+        posts.add_comment(permalink, name, email, body)
+
+        bottle.redirect("/post/" + permalink)
+
+
+@bottle.get("/post_not_found")
+def post_not_found():
+    return "Sorry, post not found"
+
+
+# Displays the form allowing a user to add a new post. Only works for logged in users
+@bottle.get('/newpost')
+def get_newpost():
+
+    cookie = bottle.request.get_cookie("session")
+    username = sessions.get_username(cookie)  # see if user is logged in
+    if username is None:
+        bottle.redirect("/login")
+
+    return bottle.template("newpost_template", dict(subject="", body = "", errors="", tags="", username=username))
+
+#
+# Post handler for setting up a new post.
+# Only works for logged in user.
+@bottle.post('/newpost')
+def post_newpost():
+    title = bottle.request.forms.get("subject")
+    post = bottle.request.forms.get("body")
+    tags = bottle.request.forms.get("tags")
+
+    cookie = bottle.request.get_cookie("session")
+    username = sessions.get_username(cookie)  # see if user is logged in
+    if username is None:
+        bottle.redirect("/login")
+
+    if title == "" or post == "":
+        errors = "Post must contain a title and blog entry"
+        return bottle.template("newpost_template", dict(subject=cgi.escape(title, quote=True), username=username,
+                                                        body=cgi.escape(post, quote=True), tags=tags, errors=errors))
+
+    # extract tags
+    tags = cgi.escape(tags)
+    tags_array = extract_tags(tags)
+
+    # looks like a good entry, insert it escaped
+    escaped_post = cgi.escape(post, quote=True)
+
+    # substitute some <p> for the paragraph breaks
+    newline = re.compile('\r?\n')
+    formatted_post = newline.sub("<p>", escaped_post)
+
+    permalink = posts.insert_entry(title, formatted_post, tags_array, username)
+
+    # now bottle.redirect to the blog permalink
+    bottle.redirect("/post/" + permalink)
+
+
+# displays the initial blog signup form
+@bottle.get('/signup')
+@bottle.view('signup')
+def present_signup():
+    return dict(username="", password="",
+                password_error="",
+                email="", username_error="", email_error="",
+                verify_error ="",year=datetime.now().year)
+
+# displays the initial blog login form
+@bottle.get('/login')
+def present_login():
+    return bottle.template("login",
+                           dict(username="", password="",
+                                login_error=""))
+
+# handles a login request
+@bottle.post('/login')
+def process_login():
+
+    username = bottle.request.forms.get("username")
+    password = bottle.request.forms.get("password")
+
+    print "user submitted ", username, "pass ", password
+
+    user_record = users.validate_login(username, password)
+    if user_record:
+        # username is stored in the user collection in the _id key
+        session_id = sessions.start_session(user_record['_id'])
+
+        if session_id is None:
+            bottle.redirect("/internal_error")
+
+        cookie = session_id
+
+        # Warning, if you are running into a problem whereby the cookie being set here is
+        # not getting set on the redirect, you are probably using the experimental version of bottle (.12).
+        # revert to .11 to solve the problem.
+        bottle.response.set_cookie("session", cookie)
+
+        bottle.redirect("/welcome")
+
+    else:
+        return bottle.template("login",
+                               dict(username=cgi.escape(username), password="",
+                                    login_error="Invalid Login"))
+
+
+@bottle.get('/internal_error')
+@bottle.view('error_template')
+def present_internal_error():
+    return {'error':"System has encountered a DB error"}
+
+
+@bottle.get('/logout')
+def process_logout():
+
+    cookie = bottle.request.get_cookie("session")
+
+    sessions.end_session(cookie)
+
+    bottle.response.set_cookie("session", "")
+
+
+    bottle.redirect("/signup")
+
+
+@bottle.post('/signup')
+@bottle.view('signup')
+def process_signup():
+
+    email = bottle.request.forms.get("email")
+    username = bottle.request.forms.get("username")
+    password = bottle.request.forms.get("password")
+    verify = bottle.request.forms.get("verify")
+
+    # set these up in case we have an error case
+    errors = {'username': cgi.escape(username),
+              'email': cgi.escape(email),
+              'year':datetime.now().year}
+    if validate_signup(username, password, verify, email, errors):
+
+        if not users.add_user(username, password, email):
+            # this was a duplicate
+            errors['username_error'] = "Username already in use. Please choose another"
+            return errors
+
+        session_id = sessions.start_session(username)
+        print session_id
+        bottle.response.set_cookie("session", session_id)
+        bottle.redirect("/welcome")
+    else:
+        print "user did not validate"
+        return errors
+
+
+
+@bottle.get("/welcome")
+def present_welcome():
+    # check for a cookie, if present, then extract value
+
+    cookie = bottle.request.get_cookie("session")
+    username = sessions.get_username(cookie)  # see if user is logged in
+    if username is None:
+        print "welcome: can't identify user...redirecting to signup"
+        bottle.redirect("/signup")
+
+    return bottle.template("welcome", {'username': username})
+
